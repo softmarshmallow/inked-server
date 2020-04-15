52932c30ec3bbf2e3b887c0f54d2c9fb6a2722fa
woojoo <woojoo@softmarshmallow.com>
2020-04-16 00:51:03 +0900

    🥳 cors fix

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,7 @@ const app = express();
 // app.use(cors());
 app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Headers", "Authorization");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Authorization");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, OPTIONS")
     next();
 });
