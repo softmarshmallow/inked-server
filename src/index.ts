5dcd1323ec50a880003e25ad0f90d8ab45902071
woojoo <woojoo@softmarshmallow.com>
2020-04-19 17:32:02 +0900

    😠 try-fix cors error

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,8 @@ const app = express();
 // app.use(cors());
 app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With, Authorization");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, OPTIONS")
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
     next();
 });
 
